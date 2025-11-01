e419571a88cf24fede3bf47aa4d8674f48ace955
shubham-nawale <shu.nawale@gmail.com>
2025-11-01 07:26:38 +0530

    added  categoryDetails.ts

---
+++ src/data/categoryDetails.ts
@@ -0,0 +1,56 @@
+// src/data/categoryDetails.ts
+
+export const categoryDetails = {
+    HAIR_AND_CARE: {
+      shopByCategory: [
+        "Shampoos",
+        "Conditioners",
+        "Hair Masks",
+        "Hair Oils",
+        "Serums",
+        "Hair Colors",
+      ],
+      shopByConcern: [
+        "Hair Fall",
+        "Dandruff",
+        "Frizz Control",
+        "Colored Hair",
+        "Dry Hair",
+      ],
+    },
+    BATH_AND_BODYCARE: {
+      shopByCategory: [
+        "Body Mists",
+        "Body Wash",
+        "Handmade Soaps",
+        "Dusting Powders",
+        "Bath Oils",
+        "Body Lotions",
+      ],
+      shopByConcern: [
+        "Dry Skin",
+        "Tan Removal",
+        "Sun Protection",
+        "Stretch Marks",
+      ],
+    },
+    SKINCARE: {
+      shopByCategory: [
+        "Moisturizers",
+        "Night Creams",
+        "Face Wash",
+        "Scrubs",
+        "Toners",
+        "Serums",
+      ],
+      shopByConcern: [
+        "Anti-Ageing",
+        "Pigmentation",
+        "Acne",
+        "Dark Circles",
+        "Sun Damage",
+      ],
+    },
+    // Add others...
+  };
+  
